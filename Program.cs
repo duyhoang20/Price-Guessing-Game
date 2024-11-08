dd76c9600ae546368ce343109405841a4a4436a0
duyhoang20 <phamduyhoang0107@gmail.com>
2024-11-08 20:03:01 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using PriceGuessingGame.Managers;
+using System;
+
+namespace PriceGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            try
+            {
+                GameManager gameManager = new GameManager();
+                gameManager.StartGame();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Lỗi khởi động chương trình: {ex.Message}");
+            }
+        }
+    }
+}
